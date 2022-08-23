b1c251968b4694383c5f1f584fcae8eca8d95900
masekjakub <masekjak02@gmail.com>
2022-08-23 22:05:00 +0200

    refactoring

--- PhotoOrganizer.py
+++ PhotoOrganizer.py
@@ -6,14 +6,10 @@ import re
 
 
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
     OKGREEN = '\033[92m'
     WARNING = '\033[93m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
-    BOLD = '\033[1m'
 
 
 IMGTYPES = ('.img', '.jpg', '.JPG', '.jpeg', 'JPEG',
